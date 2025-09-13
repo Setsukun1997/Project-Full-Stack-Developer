fe99e01583e7297a0a3de15bda56525c93835365
Setsukun1997 <tety12308@hotmail.com>
2025-09-13 11:14:00 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,11 @@ app.use(express.json());
 const connectDB = require('./mongoose');
 connectDB();
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('✅ Connected to MongoDB'))
-  .catch((err) => console.error('❌ MongoDB connection error:', err));
-
 app.use('/api/tasks', require('./routes/tasks'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
 
 
 
