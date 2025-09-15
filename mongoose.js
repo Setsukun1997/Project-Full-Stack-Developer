dfafbd7fb654f62aa07138eefc1ce52a6164e13c
Setsukun1997 <tety12308@hotmail.com>
2025-09-15 17:36:26 +0700

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,15 +1,19 @@
-// mongoose.js
-const mongoose = require('mongoose');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI=mongodb+srv://Setsukino:Setsu1652@cluster0.dnrspqb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0);
-    console.log('✅ MongoDB connected');
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB connected');
   } catch (err) {
-    console.error('❌ MongoDB connection error:', err);
+    console.error('MongoDB connection error:', err.message);
   }
 };
 
 module.exports = connectDB;
+
+
 
