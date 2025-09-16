69f52adc8dd0b030c32e6c8480a6f8803d1ff2bb
Setsukun1997 <tety12308@hotmail.com>
2025-09-16 11:03:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+
+const connectDB = require('./mongoose');
+const taskRoutes = require('./routes/task');
+
+const app = express();
+
 app.use(cors({
   origin: 'https://project-full-stack-developer.vercel.app',
   credentials: true,
 }));
 
-require('dotenv').config();
-
-const connectDB = require('./mongoose'); 
-const taskRoutes = require('./routes/task');
-
-const app = express();
-app.use(cors());
 app.use(express.json());
-
 connectDB();
-
 app.use('/api/tasks', taskRoutes);
+
 app.get('/', (req, res) => {
   res.send('Backend is running!');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
 
 
 
