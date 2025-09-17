3acde1c267fccf73c9747f480be7a7dfa9abbd38
Setsukun1997 <tety12308@hotmail.com>
2025-09-17 17:08:36 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const connectDB = require('./mongoose');
+const taskRoutes = require('./routes/task');
+
+const app = express();
+
+app.use(cors({
+  origin: 'https://project-full-stack-developer.vercel.app',
+  credentials: true,
+}));
+
+app.use(express.json());
+connectDB();
+app.use('/api/tasks', taskRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
