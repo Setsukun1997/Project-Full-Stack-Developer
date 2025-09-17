3a1714c0a61fb7b4dd508655153dca2e53f9b80c
Setsukun1997 <tety12308@hotmail.com>
2025-09-17 14:11:49 +0700

    Delete AxiosInstance.js

--- AxiosInstance.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'https://project-full-stack-developer.onrender.com', // 🔗 Backend URL (Render)
-  withCredentials: true,
-});
-
-export default instance;
