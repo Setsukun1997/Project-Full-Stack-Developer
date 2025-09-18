5d75cda69508e749ab289c0240d84718877151ed
Setsukun1997 <tety12308@hotmail.com>
2025-09-18 10:58:45 +0700

    Update task.js

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Task = require('../model/task');
+const Task = require('./backend/model/task');
 
 router.get('/', async (req, res) => {
   const tasks = await Task.find();
