2b7583dec8b6c57241c11ad3638c2c7ea4e4da8a
Setsukun1997 <tety12308@hotmail.com>
2025-09-18 11:18:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const connectDB = require('./backend/config/mongoose');
 const taskRoutes = require('./backend/routes/task');
-const mongoose = require('./backend/config/mongoose');
 
 const app = express();
 
@@ -14,8 +14,10 @@ connectDB();
 app.use('/api/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Backend is running!');
+  res.send('✅ Backend is running!');
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+
